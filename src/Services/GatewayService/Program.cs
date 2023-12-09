1d28eb08a5e482e930fb4766e075fe68390f14a1
DavidKaralinski <dadiakarol2@gmail.com>
2023-12-09 15:23:44 +0300

    feature: add gateway service
    Added gateway service to the solution and configured it according to the existing microservices api

---
+++ src/Services/GatewayService/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.Authority = configuration["IdentityServiceUrl"];
+        opt.TokenValidationParameters.ValidateAudience = false;
+        opt.TokenValidationParameters.NameClaimType = "user_name";
+        opt.RequireHttpsMetadata = false;
+    });
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
